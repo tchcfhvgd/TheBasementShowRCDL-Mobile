a5ea421181bce301484e02891a8afe1275249206
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-02-22 10:04:14 +0800

    Add files via upload

--- source/mobile/TouchPad.hx
+++ source/mobile/TouchPad.hx
@@ -88,7 +88,7 @@ class TouchPad extends MobileInputManager implements IMobileControls
 	 * @param   DPadMode     The D-Pad mode. `LEFT_FULL` for example.
 	 * @param   ActionMode   The action buttons mode. `A_B_C` for example.
 	 */
-	public function new(DPad:String, Action:String, ?Extra:ExtraActions = SINGLE)
+	public function new(DPad:String, Action:String, ?Extra:ExtraActions = NONE)
 	{
 		super();
 
@@ -125,13 +125,16 @@ class TouchPad extends MobileInputManager implements IMobileControls
 			case SINGLE:
 				if(PlayState.qqqeb)
 				{
-				add(buttonExtra = createButton(0, FlxG.height - 137, 's', 0xFF0066FF));
-				setExtrasPos();
+				if (MobileData.mode != 3)
+				{
+				add(buttonExtra = createButton((DPad == 'LEFT_FULL') ? 1149 : 0, FlxG.height - 137, 's', 0xFF0066FF));
+				//setExtrasPos();
+				}
 				}
 			case DOUBLE:
 				add(buttonExtra = createButton(0, FlxG.height - 137, 's', 0xFF0066FF));
 				add(buttonExtra2 = createButton(FlxG.width - 132, FlxG.height - 137, 'g', 0xA6FF00));
-				setExtrasPos();
+				//setExtrasPos();
 			case NONE: // nothing
 		}
 
